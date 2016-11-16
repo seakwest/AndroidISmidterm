6e4465fe77bb1bf81fa34712e8e7d8f59f4bccac
seakwest <seakwest@gmail.com>
2016-11-16 11:31:39 -0800

    Initial commit

---
+++ app/src/main/java/com/evaristo/androidismidterm/MainActivity.java
@@ -0,0 +1,232 @@
+package com.evaristo.androidismidterm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.content.SharedPreferences.Editor;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity implements TextView.OnEditorActionListener,
+        /*SeekBar.OnSeekBarChangeListener,*/ Spinner.OnItemSelectedListener, View.OnKeyListener {
+
+
+    //variable declaration for widgts
+    private EditText weightEditText;
+    private EditText nameEditText;
+    private TextView milesTextView;
+    private TextView caloriesTextView;
+    private TextView bMITextView;
+    private SeekBar milesSeekBar;
+    private Spinner feetSpinner;
+    private Spinner inchesSpinner;
+
+    // define the SharedPreferences object
+    private SharedPreferences savedValues;
+
+    //instance variables
+    private int weight;
+    private String weightString;
+    private int miles;
+    // private float caloriesBurned;
+    private double caloriesBurned; //android studio told me it wanted this to be a double and not a float
+    private float bmi;
+    private int feet;
+    private int inches;
+    private int height;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //get references to widgets
+        weightEditText = (EditText) findViewById(R.id.weightEditText);
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        milesTextView = (TextView) findViewById(R.id.milesTextView);
+        caloriesTextView = (TextView) findViewById(R.id.caloriesTextView);
+        bMITextView = (TextView) findViewById(R.id.bMITextView);
+        milesSeekBar = (SeekBar) findViewById(R.id.milesSeekBar);
+        feetSpinner = (Spinner) findViewById(R.id.feetSpinner);
+        inchesSpinner = (Spinner) findViewById(R.id.inchesSpinner);
+
+
+
+        //array adapter for feet spinner
+        ArrayAdapter feetAdapter = ArrayAdapter.createFromResource(this, R.array.feet_Array, android.R.layout.simple_spinner_item);
+        feetAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        feetSpinner.setAdapter(feetAdapter);
+
+        //adapter for inches
+        ArrayAdapter inchesAdapter = ArrayAdapter.createFromResource(this, R.array, android.R.layout.simple_spinner_item);
+        inchesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        inchesSpinner.setAdapter(inchesAdapter);
+
+
+        //set the listeners
+        weightEditText.setOnEditorActionListener(this);
+        weightEditText.setOnKeyListener(this);
+        milesSeekBar.setOnSeekBarChangeListener(this);
+        milesSeekBar.setOnKeyListener(this);
+        feetSpinner.setOnItemSelectedListener(this);
+        inchesSpinner.setOnItemSelectedListener(this);
+
+        //shared references
+        savedValues = getSharedPreferences("Saved Values", MODE_PRIVATE);
+
+        //seekbar inner class
+        class MilesSeekbar extends SeekBar {
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                milesTextView.setText(progress);
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                calculateAndDisplay();
+
+                }
+
+    }
+
+
+
+
+
+
+    @Override
+    public void onPause() {
+        // save the instance variables
+        Editor editor = savedValues.edit();
+        editor.putInt("weight", weight);
+        editor.putInt("miles", miles );
+        editor.putInt("feet", feet);
+        editor.putInt("inches", inches);
+        editor.commit();
+
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //get the instance variables
+        weight = savedValues.getInt("weight",weight);
+        miles = savedValues.getInt("miles",miles);
+        feet = savedValues.getInt("feet",feet);
+        inches = savedValues.getInt("inches",inches);
+
+        //set instance variables to their widgets
+        //set weight edit text
+        weightEditText.setText(weight);
+
+        //set miles seekbar
+        milesSeekBar.setProgress(miles);
+
+        //set feet spinner and inches spinner
+        int ftPos = feet -1;
+        feetSpinner.setSelection(ftPos);
+
+        int inPos = inches -1;
+        inchesSpinner.setSelection(inPos);
+
+
+    }
+
+    //event handler for editor action
+    public void calculateAndDisplay() {
+        // get the weight amount
+        weightString = weightEditText.getText().toString();
+        float weight;
+        if (weightString.equals("")) {
+            weight = 0;
+        } else {
+            weight = Float.parseFloat(weightString);
+        }
+
+
+
+        //get seekbar progress in miles
+        int progress = milesSeekBar.getProgress();
+        miles =  progress;
+
+        //calculate calories burned
+        caloriesBurned = 0.75 * weight * miles;
+        bmi = weight * 703 / ((12 * feet + inches) * (12*feet*inches));
+
+
+        //display calculation
+        NumberFormat decimal = NumberFormat.getInstance();
+        caloriesTextView.setText(decimal.format(caloriesBurned));
+        bMITextView.setText(decimal.format(bmi));
+
+
+
+
+
+    }
+
+
+
+
+
+    @Override
+    public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+        return false;
+    }
+    //Seekbar Handler
+   /* @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        //TODO Auto-generated method stub
+    }
+
+    /*@Override
+    public void onProgressChanged(SeekBar seekBar, int progress,
+                                  boolean fromUser) {
+        milesTextView.setText(progress);
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        calculateAndDisplay();
+    }*/
+
+
+    //Spinner Handler
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        calculateAndDisplay();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+        //do nothing
+    }
+
+    @Override
+    public boolean onKey(View view, int i, KeyEvent keyEvent) {
+        return false;
+    }
+}
+
